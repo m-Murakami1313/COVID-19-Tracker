ffa1bb5fac4a783753b5ad94a4b2f3029c770f10
m-Murakami1313 <mituvv@gmail.com>
2022-05-13 18:27:57 +0900

    add ページを作る要素を仮作成

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
+import { Body } from "./pages/Body/Body";
+import { Chart } from "./pages/Chart/Chart";
+import { Header } from "./pages/Header/Header";
+
 function App() {
   return (
     <div className="App">
-     
+      <Header />
+      <Body />
+      <Chart />
     </div>
   );
 }
