ca01e69f7f90dfd51ac0d7e037ac207cf7d63752
m-Murakami1313 <mituvv@gmail.com>
2022-05-21 17:20:23 +0900

    add CardsコンポーネントからCardFormにデータを送れるようにした。

--- src/organisms/Cards/Cards.tsx
+++ src/organisms/Cards/Cards.tsx
@@ -1,12 +1,16 @@
+import { memo } from "react";
 import { CardForm } from "../../UI/CardForm/CardForm";
 import styles from "./Cards.module.scss";
 
-export const Cards = () => {
+export const Cards = memo(({ data }: any) => {
+  
+  const date = new Date(data.lastUpdate).toDateString();
+  console.log(data);
   return (
     <div className={styles.cards}>
-      <CardForm />
-      <CardForm />
-      <CardForm />
+      <CardForm title="Infect" data={data.confirmed} date={date} />
+      <CardForm title="Recovered" data={data.recovered} date={date} />
+      <CardForm title="Deaths" data={data.deaths} date={date} />
     </div>
   );
-};
+});
